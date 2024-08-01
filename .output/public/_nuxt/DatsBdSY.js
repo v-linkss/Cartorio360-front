9cef3e3d651ebbc569063cc3e815b82da3aae0ba
Ewerton-Hugo <hugocd71@gmaol.com>
2024-08-01 00:02:17 -0300

    deploy

---
+++ .output/public/_nuxt/DatsBdSY.js
@@ -0,0 +1,1 @@
+import{B as n,a8 as o,au as r,C as l,a2 as i,H as c,b as u}from"./CGGPSomy.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),p=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{p as V};
