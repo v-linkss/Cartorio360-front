9be88aca9ad1ccd4366091f134eb6e76658b0b35
Ewerton-Hugo <hugocd71@gmail.com>
2024-10-25 11:41:17 -0300

    adicionando upload

---
+++ components/Biometria/Scanner copy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img
+      @click="runScanner"
+      src="../../assets/escanearFicha.png"
+      style="width: 280px; height: 120px; cursor: pointer; margin-top: 30px"
+    />
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+const acionarScanner = `${config.public.biometria}/run-scanner`;
+
+const runScanner = async () => {
+  const { status } = await useFetch(acionarScanner, {
+    method: "GET",
+  });
+};
+</script>
+
+<style scoped></style>
--- components/Biometria/Scanner.vue
+++ components/Biometria/Scanner.vue
@@ -6,15 +6,38 @@
       style="width: 280px; height: 120px; cursor: pointer; margin-top: 30px"
     />
   </div>
+  <input type="file" @change="handleFileUpload" />
+  <button @click="uploadFile()">Enviar</button>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
+var formData = new FormData();
+const upload = `${config.public.managemant}/upload`;
 const acionarScanner = `${config.public.biometria}/run-scanner`;
 
+
+
 const runScanner = async () => {
-  const { status } = await useFetch(acionarScanner, {
-    method: "GET",
+
+
+};
+const handleFileUpload = (event) => { 
+  console.log("handleFileUpload", event.target.files[0]);
+  const file = event.target.files[0];
+  formData.append("file", file);
+  formData.append("pessoa_token", useCookie('pessoa_token').value); 
+  formData.append("bucket", "cartorio-teste"); 
+  formData.append("tipo", "ficha"); 
+
+
+
+};
+function uploadFile() {
+    console.log("uploadFile");
+    const { data, error, status } = useFetch(upload, {
+    method: "POST",
+    body: formData
   });
 };
 </script>
