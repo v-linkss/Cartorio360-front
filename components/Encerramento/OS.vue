0d56ccaf6e9089e29ae9eefe105b6252e6d97611
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2025-02-17 17:46:43 -0300

    feat: botao de pagamento para hoje(work in progress)

---
+++ components/Encerramento/OS.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-dialog persistent v-model="isVisible" max-width="650">
+      <v-card >
+        <v-container>
+          <v-row class="mb-5 mt-1 ml-2">
+            <h1>O encerramento de OS não poderá ser revertido. Confirma o encerramento ?</h1>
+            <h1 style="color: red; margin-left: 18px">
+              {{ props.numero_os }}
+            </h1>
+          </v-row>
+        </v-container>
+  
+        <div class="mb-5" style="display: flex; justify-content: flex-start">
+          <div class="ml-6">
+            <v-btn
+              size="large"
+              color="red"
+              @click="closeModal"
+            >Fechar</v-btn>
+          </div>
+          <div class="ml-6">
+            <v-btn
+               size="large"
+              color="green"
+              @click="cancelarOrdemServ"
+            >Salvar</v-btn>
+          </div>
+        </div>
+      </v-card>
+    </v-dialog>
+  </template>
+  
+  <script setup>
+  import { useVuelidate } from "@vuelidate/core";
+  import { helpers, required } from "@vuelidate/validators";
+  
+  const props = defineProps({
+    show: Boolean,
+    numero_os: Number,
+    ordemserv_token: String,
+  });
+  
+  const isVisible = ref(props.show);
+  const config = useRuntimeConfig();
+  const cartorio_token = ref(useCookie("user-data").value.cartorio_token).value;
+  const usuario_token = useCookie("auth_token").value;
+  const analisaCancelamento = `${config.public.managemant}/analisaCancelamento`;
+  const cancelarOs = `${config.public.managemant}/cancelaOs`;
+  
+  const state = reactive({
+    motivo: null,
+  });
+  
+  const analiseCancela = ref(null)
+  
+  const emit = defineEmits(["close"]);
+  
+  const rules = {
+    motivo: {
+      required: helpers.withMessage("O campo é obrigatorio", required),
+    },
+  };
+  
+  const v$ = useVuelidate(rules, state);
+  
+  watch(
+    () => props.show,
+    (newVal) => {
+      isVisible.value = newVal;
+    }
+  );
+  const closeModal = () => {
+    isVisible.value = false;
+    emit("close");
+  };
+  
+  const analisaCancelamentoOs = async () => {
+    const { data, error } = await useFetch(`${analisaCancelamento}`, {
+        method: "POST",
+        body: { ordemserv_token:props.ordemserv_token },
+      });
+  
+      analiseCancela.value = data.value.mensagem
+  };
+  
+  analisaCancelamentoOs()
+  
+  const cancelarOrdemServ = async () => {
+    if (await v$.value.$validate()) {
+      const { status } = await useFetch(`${cancelarOs}`, {
+        method: "POST",
+        body: { usuario_token: usuario_token, motivo:state.motivo, ordemserv_token:props.ordemserv_token },
+      });
+      if(status.value ==='success'){
+        window.location.reload()
+        closeModal()
+      }
+    }
+  };
+  </script>
+  
--- pages/caixas/caixasRecebimentoOs/index.vue
+++ pages/caixas/caixasRecebimentoOs/index.vue
@@ -42,17 +42,17 @@
             />
           </div>
           <div
-            :class="{ disabled: !item.btn_cancelar }"
+            :class="{ disabled: !item.btn_encerrar }"
             @click="
-              item.btn_cancelar
-                ? redirectToCancelamento(item.numero, item.token)
+              item.btn_encerrar
+                ? encerrarOS(item.id)
                 : null
             "
             title="Cancelamento"
           >
             <img
-              style="width: 30px; height: 30px"
-              src="../../../assets/salvar.png"
+              style="width: 30px; height: 30px; cursor: pointer"
+              src="../../../assets/visualizar.png"
               alt="Visualizar"
               title="Visualizar"
             />
@@ -66,6 +66,7 @@
       :ordem="selectedOrder"
       @close="isModalRecebimentoOpen = false"
     />
+    <ModalConfirmacao cond-message="O encerramento de OS não poderá ser revertido. Confirma o encerramento ?" :show="isModalCancelamentoOpen" close="isModalCancelamentoOpen = false"/>
   </v-container>
   <v-rows>
     <v-cols>
@@ -82,6 +83,7 @@ import { ref, computed, onMounted } from "vue";
 const config = useRuntimeConfig();
 const { $toast } = useNuxtApp();
 const listarOSCaixas = `${config.public.managemant}/listarOSCaixas`;
+const encerrarOs = `${config.public.managemant}/updateOrdensServico`;
 
 const nome_usuario = useCookie("caixa-service").value.usuario_nome;
 const data = useCookie("caixa-service").value.data;
@@ -92,6 +94,7 @@ const searchApresentante = ref("");
 const selectedOrder = ref({});
 const numero_os = ref(null);
 const isModalRecebimentoOpen = ref(false);
+const isModalCancelamentoOpen = ref(false);
 
 const goBack = () => {
 navigateTo('/caixas/lista')
@@ -131,6 +134,37 @@ async function caixaOsDataPayload() {
   }
 }
 
+function getCurrentDate() {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const MM = String(today.getMonth() + 1).padStart(2, "0");
+  const dd = String(today.getDate()).padStart(2, "0");
+  return `${yyyy}-${MM}-${dd}`;
+}
+
+async function encerrarOS(id){
+  try {
+    const response = await $fetch(`${encerrarOs}/${id}`, {
+      method: "PUT",
+      body: {
+        dt_pagto: getCurrentDate()
+      },
+    });
+    console.log('res', response)
+    if (response && Array.isArray(response)) {
+      caixaRecebeOsItems.value = response;
+    } else {
+      $toast.error("Nenhum dado retornado da API.");
+      console.error("Nenhum dado retornado da API.");
+    }
+  } catch (error) {
+    const errorMessage =
+      error.message || "Erro ao buscar dados da API. Tente novamente.";
+    $toast.error(errorMessage);
+    console.error(errorMessage);
+  }
+} 
+
 onMounted(() => {
   caixaOsDataPayload();
 });
@@ -152,7 +186,8 @@ const filteredItems = computed(() => {
 });
 console.log('items:',filteredItems)
 function redirectToCancelamento(numero, token) {
-  // Lógica para redirecionar ao cancelamento
+  isModalCancelamentoOpen.value = true;
+  
   console.log("Cancelando OS:", { numero, token });
 }
 
