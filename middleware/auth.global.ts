20fe6f62fc2e26726fe99f0a1f0918ce2b75861c
Ewerton-Hugo <hugocd71@gmail.com>
2025-06-23 08:51:34 -0300

    fix: a rota fde chat_bot não precisa mais de autenticação

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,8 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const tokenCookie = useCookie("auth_token");
+  const tokenCookie = useCookie('auth_token');
   const token = tokenCookie.value;
 
-  if (!token && to.path !== "/login") {
+  // Lista de rotas públicas (que não precisam de autenticação)
+  const publicRoutes = ["/login", "/chat_bot"]; // Adicione aqui as rotas que deseja excluir
+
+  // Se a rota atual for pública, não faz a verificação de autenticação
+  if (publicRoutes.includes(to.path)) {
+    return;
+  }
+
+  // Se o token não existir e a rota não for pública, redireciona para "/login"
+  if (!token) {
     return navigateTo("/login");
   }
 });
