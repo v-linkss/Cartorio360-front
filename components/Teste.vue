4a0c5d7debbe83e8c685b925ca1a32d799b535ff
Ewerton-Hugo <hugocd71@gmail.com>
2025-07-02 19:35:57 -0300

    fix: historico do chat, desativação do contador de tempo o cioso qaundo o atendimento é humano, icone de notificação

--- components/Teste.vue
+++ components/Teste.vue
@@ -16,6 +16,7 @@ registerLicense(`${config.public.docEditor}`);
 </script>
 
 
+
 <style>
 @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
 @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
