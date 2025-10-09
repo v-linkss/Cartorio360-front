645d4e436fbc8ec6c1721a1cc632c02f871782ec
Ewerton-Hugo <hugocd71@gmail.com>
2025-10-09 10:25:05 -0300

    refact: censec layout

--- pages/integracao/censec.vue
+++ pages/integracao/censec.vue
@@ -1,85 +1,86 @@
 <template>
-  <v-container class="py-6">
-    <v-row class="mb-5">
-      <v-col cols="12" class="mx-auto" style="max-width: 1200px;">
-        <h1 class="text-h4 font-weight-bold">Integração CENSEC</h1>
-      </v-col>
+  <v-container class="mt-5">
+    <v-row class="mb-10 ">
+      <h1>Integração CENSEC</h1>
     </v-row>
-    <v-card elevation="2" class="mx-auto" max-width="1200">
 
-      <v-card-text class="pa-6">
-        <!-- Seção de Período -->
-        <div class="mb-6">
-          <h3 class="text-h6 mb-4">Período de Consulta</h3>
+    <div>
+      <!-- Seção de Período -->
+      <div class="mb-6">
+        <h3 class="text-h6 mb-2">Período de Consulta</h3>
+        <v-row>
+          <v-col cols="12" md="1">
+            <v-text-field v-model="state.data_inicio" label="Data Inicio" placeholder="dd/mm/yyyy"
+              v-mask="'##/##/####'"></v-text-field>
+          </v-col>
+          <v-col cols="12" md="1">
+            <v-text-field v-model="state.data_fim" label="Data Fim" placeholder="dd/mm/yyyy"
+              v-mask="'##/##/####'"></v-text-field>
+          </v-col>
+        </v-row>
+      </div>
+
+      <v-divider class="my-6"></v-divider>
+
+      <!-- Seção de Tipo de Validação -->
+      <div class="mb-6">
+        <v-row>
+          <v-col cols="12" , sm="6" md="4"></v-col>
+          <v-col cols="12" , sm="6" md="4">
+            <h3 class="text-h6 mb-4">Gerar Arquivo</h3>
+
+          </v-col>
+        </v-row>
+        <v-radio-group v-model="state.validar" class="mt-2">
           <v-row>
-            <v-col cols="12" md="6">
-              <v-text-field v-model="state.data_inicio" label="Data Início" placeholder="dd/mm/yyyy"
-                v-mask="'##/##/####'" prepend-inner-icon="mdi-calendar-start" variant="outlined"
-                density="comfortable"></v-text-field>
+            <v-col cols="12" sm="6" md="4" lg="2-4">
+              <v-radio label="VALIDAR ATOS" value="ATOS" color="primary"></v-radio>
             </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field v-model="state.data_fim" label="Data Fim" placeholder="dd/mm/yyyy" v-mask="'##/##/####'"
-                prepend-inner-icon="mdi-calendar-end" variant="outlined" density="comfortable"></v-text-field>
+            <v-col cols="12" sm="6" md="4" lg="2-4">
+              <v-radio label="CESDI" value="CESDI" color="primary"></v-radio>
             </v-col>
-          </v-row>
-        </div>
-
-        <v-divider class="my-6"></v-divider>
-
-        <!-- Seção de Tipo de Validação -->
-        <div class="mb-6">
-          <h3 class="text-h6 mb-4">Tipo de Validação</h3>
-          <v-radio-group v-model="state.validar" class="mt-2">
-            <v-row>
-              <v-col cols="12" sm="6" md="4" lg="2-4">
-                <v-radio label="VALIDAR ATOS" value="ATOS" color="primary"></v-radio>
-              </v-col>
-              <v-col cols="12" sm="6" md="4" lg="2-4">
-                <v-radio label="CESDI" value="CESDI" color="primary"></v-radio>
-              </v-col>
-              <v-col cols="12" sm="6" md="4" lg="2-4">
-                <v-radio label="CEP" value="CEP" color="primary"></v-radio>
-              </v-col>
-              <v-col cols="12" sm="6" md="4" lg="2-4">
-                <v-radio label="RCTO" value="RCTO" color="primary"></v-radio>
-              </v-col>
-              <v-col cols="12" sm="6" md="4" lg="2-4">
-                <v-radio label="CCN" value="CCN" color="primary"></v-radio>
-              </v-col>
-            </v-row>
-          </v-radio-group>
-        </div>
-
-        <v-divider class="my-6"></v-divider>
-
-        <!-- Seção de Nome do Arquivo e Ação -->
-        <div class="mb-4">
-          <h3 class="text-h6 mb-4">Arquivo</h3>
-          <v-row align="center">
-            <v-col cols="12" md="8">
-              <v-text-field label="Nome do arquivo" v-model="state.file_name"
-                :disabled="!state.validar || state.validar === 'ATOS'" prepend-inner-icon="mdi-file-document-outline"
-                variant="outlined" density="comfortable" hint="Selecione um tipo de validação primeiro"
-                persistent-hint></v-text-field>
+            <v-col cols="12" sm="6" md="4" lg="2-4">
+              <v-radio label="CEP" value="CEP" color="primary"></v-radio>
             </v-col>
-            <v-col cols="12" md="4" class="d-flex justify-end">
-              <SaveButton :text="'Enviar'" :onSave="enviaDadosCensec" class="px-8" />
+            <v-col cols="12" sm="6" md="4" lg="2-4">
+            </v-col>
+            <v-col cols="12" sm="6" md="4" lg="2-4">
+              <v-radio label="RCTO" value="RCTO" color="primary"></v-radio>
+            </v-col>
+            <v-col cols="12" sm="6" md="4" lg="2-4">
+              <v-radio label="CCN" value="CCN" color="primary"></v-radio>
             </v-col>
           </v-row>
-        </div>
-      </v-card-text>
-    </v-card>
+        </v-radio-group>
+      </div>
 
-    <!-- Card de Resultados -->
-    <v-card elevation="2" class="mx-auto mt-6" max-width="1200" v-if="dadosCensec">
-      <v-card-title class="bg-success text-white pa-4">
+      <v-divider class="my-6"></v-divider>
+
+      <!-- Seção de Nome do Arquivo e Ação -->
+      <div class="mb-4">
+        <h3 class="text-h6 mb-4">Arquivo</h3>
+        <v-row align="center">
+          <v-col cols="12" md="8">
+            <v-text-field label="Nome do arquivo" v-model="state.file_name"
+              :disabled="!state.validar || state.validar === 'ATOS'"></v-text-field>
+          </v-col>
+          <v-col cols="12" md="4" class="d-flex justify-end">
+            <SaveButton :text="'Enviar'" :onSave="enviaDadosCensec" class="px-8" />
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+
+    <!-- Seção de Resultados -->
+    <div class="mt-6" v-if="dadosCensec">
+      <div class="bg-success text-white pa-4 mb-4 rounded">
         <v-icon class="mr-2">mdi-check-circle</v-icon>
         Dados CENSEC Retornados
-      </v-card-title>
-      <v-card-text class="pa-6">
-        <pre class="bg-grey-lighten-4 pa-4 rounded">{{ JSON.stringify(dadosCensec, null, 2) }}</pre>
-      </v-card-text>
-    </v-card>
+      </div>
+      <div class="bg-grey-lighten-4 pa-4 rounded">
+        <pre>{{ JSON.stringify(dadosCensec, null, 2) }}</pre>
+      </div>
+    </div>
   </v-container>
 </template>
 
