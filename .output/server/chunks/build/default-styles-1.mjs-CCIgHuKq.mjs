d80719743fe59871ecd9245af6c8fd33e92435e3
Ewerton-Hugo <hugocd71@gmail.com>
2025-10-14 09:54:08 -0300

    refact tipo-selo table

--- .output/server/chunks/build/default-styles-1.mjs-CCIgHuKq.mjs
+++
@@ -1,4 +0,0 @@
-const default_vue_vue_type_style_index_0_scoped_3dbb8198_lang = ".menu-texts .v-btn[data-v-3dbb8198]{color:#429946}.user[data-v-3dbb8198]{background-color:#fff;color:#73777a}";
-
-export { default_vue_vue_type_style_index_0_scoped_3dbb8198_lang as d };
-//# sourceMappingURL=default-styles-1.mjs-CCIgHuKq.mjs.map
