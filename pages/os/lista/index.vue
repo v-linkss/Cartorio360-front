5428bb78b733292f6a6ae924d97dd7d1017b4556
Ewerton-Hugo <hugocd71@gmail.com>
2025-11-28 17:00:02 -0300

    refact: rota de situcações da os

--- pages/os/lista/index.vue
+++ pages/os/lista/index.vue
@@ -230,7 +230,7 @@ const { $toast } = useNuxtApp();
 const allUsuarios = `${config.public.managemant}/listarUsuarios`;
 const allServicos = `${config.public.managemant}/listarOrdensServico`;
 const allTiposAtos = `${config.public.managemant}/tipoAtos`;
-const situacaoAto = `${config.public.auth}/service/gerencia/lista_sit_atos`;
+const situacaoAto = `${config.public.auth}/service/gerencia/lista_sit_os`;
 const imprimirRecibo = `${config.public.auth}/service/gerencia/emitir_recibo`;
 const router = useRouter();
 
